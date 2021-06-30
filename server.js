6294b9bb48a56535d8303bb08f8ed9ae96c0c26a
minhhung <minhhunghuynh1106@gmail.com>
2021-06-30 10:58:47 -0700

    add heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
