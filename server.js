a4322366c80e0d31da40a615d4f50d838540453c
minhhung <minhhunghuynh1106@gmail.com>
2021-08-15 22:55:38 +0700

    remove comments

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // <== Will be created later
+const router = jsonServer.router("db.json"); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; // <== You can change the port
+const port = process.env.PORT || 3200;
 
 server.use(middlewares);
 server.use(router);
